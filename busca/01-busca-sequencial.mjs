07f1a0c9d088a6a8c4193d8d6df94c0ab6b9e2fd
Feliphe Faleiros Alarcon <feliphefaleiros@hotmail.com>
2021-08-23 01:57:12 -0300

    (16/08/21) Término da revisão de JS e início do algorito de busca sequencial

---
+++ busca/01-busca-sequencial.mjs
@@ -0,0 +1,30 @@
+/**     ALGORÍTIMO DE BUSCA SEQUENCIAL 
+    Percorre o vetor, verificando se cada um dos elementos corresponde 
+    ao valor busca. Ao encontrar um correspondência, retorna 
+    a posição onde o valor de busca está no vetor.
+ 
+    Caso o valor de busca não exista no vetor, retorna o valor convencional -1.
+    */
+
+function buscaSequencial(vetor, valorBusca){
+    //Percurso do vetor com for tradicional 
+    for(let i = 0; i < vetor.length; i++){
+        if(vetor[i] === valorBusca) return i
+    }
+    return -1   //valorBusca não existe em vetor
+}
+
+const frutas = ['laranja', 'maça', 'uva', 'pera', 'jabuticaba', 'limão', 'mamão']
+
+console.log('Posição de uva:', buscaSequencial(frutas,'uva'))
+console.log('Posição de laranja:', buscaSequencial(frutas,'laranja'))
+console.log('Posição de mamão:', buscaSequencial(frutas,'mamão'))
+console.log('Posição de abacate:', buscaSequencial(frutas,'abacate'))
+
+//Número de comparações em um vetor de n elementos:
+//Quando valorBusca é encontrado: pos + 1
+//Quando valorBusca não é encontrado: n
+
+import { nomes } from '../data/vetor-nomes.mjs'
+
+console.log('Posição de Feliphe:', buscaSequencial(nomes, 'FELIPHE'))
