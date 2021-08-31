f9e7c243b43f79ce82f90f9df33dcd745a57b202
Feliphe Faleiros Alarcon Lima <feliphe.lima@consorcioluiza.intranet>
2021-08-31 12:42:39 -0300

    Inicio do Selecion Sort

---
+++ ordenacao/06-bubble-sort-obj.mjs
@@ -0,0 +1,54 @@
+let pass, comps, trocas
+
+/*
+    fnComps() passará à função externa os dois elementos adjacentes do
+    vetor e retornará:
+    -true: se o primeiro elemento for maior que o segundo
+    -false:
+*/
+function bubbleSort(vetor, fnComp) {
+    pass = 0, comps = 0, trocas = 0    
+    let trocou    
+    do {
+        pass++        
+        trocou = false        
+        // Percurso for tradicional até a PENÚLTIMA posição do vetor        
+        for(let i = 0; i < vetor.length - 1; i++) {
+            comps++            
+            //if(vetor[i] > vetor[i + 1]) {
+            if(fnComp(vetor[i],vetor[i + 1])) {
+                // Efetua a troca entre os elementos por desestruturação                
+                [ vetor[i], vetor[i + 1] ] = [ vetor[i + 1], vetor[i] ]
+                trocou = true                
+                trocas++            }
+        }
+    } while(trocou)
+}
+
+import { objMotoristas } from "../data/motoristas-obj-desord.mjs"
+
+console.time("Tempo de ordenação")
+
+//Ordenado por  nome_motorista
+//bubbleSort(objMotoristas,(elem1, elem2) => elem1.nome_motorista > elem2.nome_motorista)
+
+//Ordenado por  nome_motorista em ordem DECRESCENTE
+//bubbleSort(objMotoristas,(elem1, elem2) => elem1.nome_motorista < elem2.nome_motorista)
+
+//Ordenado por  nome_motorista em ordem DECRESCENTE ignorando acentos
+//bubbleSort(objMotoristas,(elem1, elem2) => elem1.nome_motorista.localeCompare(elem2.nome_motorista, "pt-br") <= 0) //LEEEENTO
+
+//Ordenação em dois níveis: primeiro por razao_social e depois por nome_motorista
+bubbleSort(objMotoristas,(elem1, elem2) => {
+    if (elem1.razao_social === elem2.razao_social){
+        //Desempate é feito pelo nome do motorista 
+        return elem1.nome_motorista > elem2.nome_motorista
+    }
+    //Empresas diferentes, comparação direta de razao_social
+    else return elem1.razao_social > elem2.razao_social
+})
+
+console.timeEnd("Tempo de ordenação")
+
+console.log(objMotoristas)
+console.log({pass,comps,trocas})
